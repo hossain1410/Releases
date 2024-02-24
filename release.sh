ca0e4c3bcaa4200a85ad41928af90480b0926904
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-02-24 11:34:39 +0600

    Update release.sh

--- release.sh
+++ release.sh
@@ -7,9 +7,9 @@ filename=$(ls *.zip)
 # Create a tag and release using the filename (without .zip extension)
 version=${filename%.zip}
 
-git tag -a "$version" -m "Release $version"
+git tag -a "$version" -m "$version"
 
 git push origin "$version"
 
-gh release create "$version" "$filename" -t "Release $version" -n "Release notes"
+gh release create "$version" "$filename" -t "$version" -n "Release notes"
 
